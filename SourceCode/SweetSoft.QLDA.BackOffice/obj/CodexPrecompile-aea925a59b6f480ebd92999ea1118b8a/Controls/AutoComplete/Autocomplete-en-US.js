63e61c93b521854f42d6f202cc22beae82daecce
TranHuuNhatHoang <hoanggoodboy123@gmail.com>
2026-09-04 11:29:32 +0700

    fix

--- SourceCode/SweetSoft.QLDA.BackOffice/obj/CodexPrecompile-aea925a59b6f480ebd92999ea1118b8a/Controls/AutoComplete/Autocomplete-en-US.js
+++
@@ -1,4 +0,0 @@
-﻿UCAutocomplete.Language = {
-    NO_RESULTS: 'No match, please try another keyword!',
-    LIMITED_RESULTS: 'Up to {0} results, please enter a longer keyword!'
-};
--- SourceCode/SweetSoft.QLDA.BackOffice/obj/CodexPrecompile-aea925a59b6f480ebd92999ea1118b8a/Controls/AutoComplete/Autocomplete-vi-VN.js
+++
@@ -1,4 +0,0 @@
-﻿UCAutocomplete.Language = {
-    NO_RESULTS: 'Không tìm thấy kết quả phù hợp, vui lòng thử từ khóa khác!',
-    LIMITED_RESULTS: 'Lên đến {0} kết quả, vui lòng nhập từ khóa dài hơn!'
-};
